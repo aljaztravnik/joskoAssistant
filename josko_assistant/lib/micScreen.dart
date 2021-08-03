6d1f4d67e16ab2b4ceaab02951a6b522778ba911
aljaztravnik <travnik.aljaz@gmail.com>
2021-08-03 12:47:41 +0200

    ne

--- josko_assistant/lib/micScreen.dart
+++ josko_assistant/lib/micScreen.dart
@@ -6,26 +6,25 @@ import 'package:flutter/foundation.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
-class MicScreen extends StatefulWidget
-{
+class MicScreen extends StatefulWidget {
   const MicScreen({Key key, @required this.device}) : super(key: key);
   final BluetoothDevice device;
   @override
   _MicScreenState createState() => _MicScreenState();
 }
 
-class _MicScreenState extends State<MicScreen>
-{
-  static const String COMMAND_SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-  static const String COMMAND_CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+class _MicScreenState extends State<MicScreen> {
+  static const String COMMAND_SERVICE_UUID =
+      "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+  static const String COMMAND_CHARACTERISTIC_UUID =
+      "beb5483e-36e1-4688-b7f5-ea07361b26a8";
   BluetoothCharacteristic commandCharacteristic;
   stt.SpeechToText _speech;
   bool _isListening = false;
   String _text = '';
 
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
     _speech = stt.SpeechToText();
     connectToDevice();
@@ -56,89 +55,122 @@ class _MicScreenState extends State<MicScreen>
     widget.device.disconnect();
   }
 
-  discoverServices() async
-  {
+  discoverServices() async {
     if (widget.device == null) {
       _Pop();
       return;
     }
 
     List<BluetoothService> services = await widget.device.discoverServices();
-    services.forEach((service)
-    {
-      if(service.uuid.toString() == COMMAND_SERVICE_UUID)
-      {
-        service.characteristics.forEach((characteristic)
-        {
-          if (characteristic.uuid.toString() == COMMAND_CHARACTERISTIC_UUID)  commandCharacteristic = characteristic;
+    services.forEach((service) {
+      if (service.uuid.toString() == COMMAND_SERVICE_UUID) {
+        service.characteristics.forEach((characteristic) {
+          if (characteristic.uuid.toString() == COMMAND_CHARACTERISTIC_UUID)
+            commandCharacteristic = characteristic;
         });
       }
     });
   }
 
-  Future<bool> _onWillPop()
-  {
-    return showDialog
-    (
-      context: context,
-      builder: (context) => new AlertDialog
-      (
-        title: Text('Are you sure?'),
-        content: Text('Do you want to disconnect device and go back?'),
-        actions: <Widget>
-        [
-          new FlatButton
-          (
-            onPressed: () => Navigator.of(context).pop(false),
-            child: new Text('No')
-          ),
-          new FlatButton
-          (
-            onPressed: () {
-              disconnectFromDevice();
-              Navigator.of(context).pop(true);
-            },
-            child: new Text('Yes')
-          ),
-        ],
-      ) ?? false
-    );
+  Future<bool> _onWillPop() {
+    return showDialog(
+        context: context,
+        builder: (context) =>
+            new AlertDialog(
+              title: Text('Are you sure?'),
+              content: Text('Do you want to disconnect device and go back?'),
+              actions: <Widget>[
+                new FlatButton(
+                    onPressed: () => Navigator.of(context).pop(false),
+                    child: new Text('No')),
+                new FlatButton(
+                    onPressed: () {
+                      disconnectFromDevice();
+                      Navigator.of(context).pop(true);
+                    },
+                    child: new Text('Yes')),
+              ],
+            ) ??
+            false);
   }
 
   _Pop() {
     Navigator.of(context).pop(true);
   }
 
-  sendCommand() async
-  {
-    await commandCharacteristic.write(utf8.encode(_text));
+  sendCommand() async {
+    // kle nared nek map z moznimi ukazi
+    List<String> turnOnOffUkazi = [
+      "lights",
+      "computer",
+    ];
+    List<String> otherUkazi = [
+      "time",
+      "pause",
+      "resume",
+    ];
+
+    String ukaz = "";
+    int i = 0;
+    int onOff = 0;
+
+    if (_text.contains("turn on"))
+      onOff = 1;
+    else if (_text.contains("turn off")) onOff = 0;
+
+    for (final u in turnOnOffUkazi) {
+      if (_text.contains(u)) {
+        ukaz = i.toString() + " " + onOff.toString();
+        i++;
+        break;
+      }
+      i++;
+    }
+
+    if (_text.contains("song") || _text.contains("music")) {
+      if (_text.contains("start")) {
+        ukaz = i.toString() + " 1";
+        i += 2;
+      } else if (_text.contains("stop")) {
+        i++;
+        ukaz = i.toString() + " 0";
+        i++;
+      }
+    }
+
+    for (final u in otherUkazi) {
+      if (_text.contains(u)) {
+        ukaz = i.toString() + " 0";
+        i++;
+        break;
+      }
+      i++;
+    }
+
+    await commandCharacteristic.write(utf8.encode(ukaz));
+    //await commandCharacteristic.write(utf8.encode(_text));
   }
 
-  void _listen() async
-  {
-    if(!_isListening)
-    {
+  void _listen() async {
+    if (!_isListening) {
       bool available = await _speech.initialize(
         onStatus: (val) => print("onStatus: $val"),
         onError: (val) => print("onError: $val"),
       );
-      if(available)
-      {
+      if (available) {
         setState(() {
           _isListening = true;
         });
         _speech.listen(
-          onResult: (val) => setState((){
+          onResult: (val) => setState(() {
             _text = val.recognizedWords;
           }),
         );
       }
-    }
-    else
-    {
+    } else {
       print("RECEIVED TEXT: $_text");
       setState(() {
-      _isListening = false;
+        _isListening = false;
       });
       _speech.stop();
       await sendCommand();
@@ -147,62 +179,72 @@ class _MicScreenState extends State<MicScreen>
   }
 
   @override
-  Widget build(BuildContext context)
-  {
-    return WillPopScope
-    (
+  Widget build(BuildContext context) {
+    return WillPopScope(
       onWillPop: _onWillPop,
-      child: Scaffold
-      (
+      child: Scaffold(
         backgroundColor: Colors.black,
-        body: Center
-        (
-          child: Column
-          (
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>
-            [
-              Text("JOŠKO ASSISTANT", style: TextStyle(color: Colors.white, fontFamily: 'OpenSans', fontSize: 35.0, fontWeight: FontWeight.bold),),
-              Padding(padding: EdgeInsets.only(bottom: 15)),
-              Row
-              (
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>
-                [
-                  RaisedButton
-                  (
-                    elevation: 5.0,
-                    onPressed: () {_listen();},
-                    shape: RoundedRectangleBorder
-                    (
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.white)
-                    ),
-                    child: Icon(_isListening ? MdiIcons.microphoneOff : MdiIcons.microphone, color: Colors.black),
-                  ),
-                  RaisedButton
-                  (
-                    elevation: 5.0,
-                    onPressed: ()
-                    {
-                      setState(() {
-                        _text = '';
-                      });
-                    },
-                    shape: RoundedRectangleBorder
-                    (
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.white)
-                    ),
-                    child: Text("CLEAR", style: TextStyle(color: Colors.black, fontFamily: 'OpenSans', fontSize: 20.0, fontWeight: FontWeight.normal),),
-                  ),
-                ]
-              ),
-              Padding(padding: EdgeInsets.only(bottom: 15)),
-              Text("Command: $_text", style: TextStyle(color: Colors.white, fontFamily: 'OpenSans', fontSize: 25.0, fontWeight: FontWeight.normal),),
-            ]
-          ),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "JOŠKO ASSISTANT",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'OpenSans',
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 15)),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      RaisedButton(
+                        elevation: 5.0,
+                        onPressed: () {
+                          _listen();
+                        },
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.white)),
+                        child: Icon(
+                            _isListening
+                                ? MdiIcons.microphoneOff
+                                : MdiIcons.microphone,
+                            color: Colors.black),
+                      ),
+                      RaisedButton(
+                        elevation: 5.0,
+                        onPressed: () {
+                          setState(() {
+                            _text = '';
+                          });
+                        },
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.white)),
+                        child: Text(
+                          "CLEAR",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'OpenSans',
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ]),
+                Padding(padding: EdgeInsets.only(bottom: 15)),
+                Text(
+                  "Command: $_text",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'OpenSans',
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.normal),
+                ),
+              ]),
         ),
       ),
     );
